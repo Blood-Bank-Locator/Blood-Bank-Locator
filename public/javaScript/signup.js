a1454d6c8962bf68fa219bc21678952c1fcf471c
nancy <gautamnancy324@gmail.com>
2023-08-04 12:17:20 +0530

    everything done

--- public/javaScript/signup.js
+++ public/javaScript/signup.js
@@ -15,29 +15,54 @@ function isValidEmail(email) {
     return false;
   }
 }
-
 function isValidPassword(password) {
-  console.log(password);
-  // const password = document.getElementById("password").value;
-  if (password.length < 8) {
-    return false;
+  const lowercaseRegex = /[a-z]/;  // Regular expression to match lowercase letters
+  const uppercaseRegex = /[A-Z]/;  // Regular expression to match uppercase letters
+  const numericRegex = /[0-9]/;    // Regular expression to match numeric characters
+
+  if (
+    lowercaseRegex.test(password) &&
+    uppercaseRegex.test(password) &&
+    numericRegex.test(password) &&
+    password.length >= 8
+  ) {
+    return true;  // Password meets the requirements
   }
 
-  // Check if password contains at least one alphabet and one numeric character
-  const hasAlphabet = /[a-zA-Z]/.test(password);
-  const hasNumber = /\d/.test(password);
-  if (!hasAlphabet || !hasNumber) {
-    return false;
-  }
-
-  // Check if password contains at least one of the special characters @, &, or -
-  const hasSpecialChar = /[@&-]/.test(password);
-  if (!hasSpecialChar) {
-    return false;
-  }
-
-  return true;
+  return false;  // Password does not meet the requirements
 }
+
+// function isValidPassword(password) {
+//   console.log(password);
+//   // const password = document.getElementById("password").value;
+//   if (password.length < 8) {
+//     return false;
+//   }
+
+//   // Check if password contains at least one alphabet and one numeric character
+//   const hasAlphabet = /[a-z]/.test(password);
+//   const hasNumber = /\d/.test(password);
+//   if (!hasAlphabet || !hasNumber) {
+//     return false;
+//   }
+
+//   // Check if password contains at least one of the special characters @, &, or -
+//   const hasSpecialChar = /[@&-]/.test(password);
+//   if (!hasSpecialChar) {
+//     return false;
+//   }
+//   const uppercaseRegex = /[A-Z]/;
+//   if ( !uppercaseRegex.test(password)){
+//     return false;  // Password meets the requirements
+//   }
+
+//   return true;
+
+  
+// }
+
+
+
 
 function isValidPhoneNumber(phoneNumber) {
   console.log(phoneNumber);
